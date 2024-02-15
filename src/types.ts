19f89278d482e7a3479fc4ea116bfd78dde26dfa
Nick Alteen <ncalteen@github.com>
2024-02-15 12:59:03 -0500

    Add types

--- src/types.ts
+++ src/types.ts
@@ -133,3 +133,54 @@ export type Runs = {
   /** Conditions for the `post` script to run */
   'post-if'?: () => boolean
 }
+
+/**
+ * Optional properties that can be sent with annotation commands (notice, error,
+ * and warning).
+ */
+export type AnnotationProperties = {
+  /** A title for the annotation. */
+  title?: string
+
+  /** The path of the file for which the annotation should be created. */
+  file?: string
+
+  /** The start line for the annotation. */
+  startLine?: number
+
+  /**
+   * The end line for the annotation. Defaults to `startLine` when `startLine`
+   * is provided.
+   */
+  endLine?: number
+
+  /**
+   * The start column for the annotation. Cannot be sent when `startLine` and
+   * `endLine` are different values.
+   */
+  startColumn?: number
+
+  /**
+   * The end column for the annotation. Cannot be sent when `startLine` and
+   * `endLine` are different values. Defaults to `startColumn` when
+   * `startColumn` is provided.
+   */
+  endColumn?: number
+}
+
+/**
+ * Options for getInput.
+ */
+export type InputOptions = {
+  /**
+   * Optional. Whether the input is required. If required and not present,
+   * will throw. Defaults to false.
+   */
+  required?: boolean
+
+  /**
+   * Optional. Whether leading/trailing whitespace will be trimmed for the
+   * input. Defaults to true.
+   */
+  trimWhitespace?: boolean
+}
