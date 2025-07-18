0b009da539ff5c1db07d7f0d0fc0d6726acfac3c
Nick Alteen <ncalteen@github.com>
2025-07-18 12:57:34 -0400

    feat: add bootstrap script requirement to NODE_OPTIONS

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -9,31 +9,46 @@
  * will parse them and register them so that modules can be resolved correctly.
  */
 
-const { existsSync, readFileSync } = require('fs')
-const { loadConfig, register } = require('tsconfig-paths')
-const { parse } = require('comment-json')
+import('fs').then(({ existsSync, readFileSync }) => {
+  import('tsconfig-paths').then(({ loadConfig, register }) => {
+    import('comment-json').then(({ parse }) => {
+      import('path').then(({ dirname }) => {
+        import('url').then(({ fileURLToPath }) => {
+          if (
+            process.env.TARGET_ACTION_PATH &&
+            process.env.TARGET_ACTION_PATH !== ''
+          ) {
+            // Check if the action has a `tsconfig.json` file.
+            if (existsSync(`${process.env.TARGET_ACTION_PATH}/tsconfig.json`)) {
+              const __dirname = dirname(fileURLToPath(import.meta.url))
 
-if (process.env.TARGET_ACTION_PATH && process.env.TARGET_ACTION_PATH !== '') {
-  // Check if the action has a `tsconfig.json` file.
-  if (existsSync(`${process.env.TARGET_ACTION_PATH}/tsconfig.json`)) {
-    // Load the `tsconfig.json` from the action directory.
-    const actionTsConfig = parse(
-      readFileSync(`${process.env.TARGET_ACTION_PATH}/tsconfig.json`, 'utf-8')
-    )
+              // Load the `tsconfig.json` from the action directory.
+              const actionTsConfig = parse(
+                readFileSync(
+                  `${process.env.TARGET_ACTION_PATH}/tsconfig.json`,
+                  'utf-8'
+                )
+              )
 
-    // Load the current `tsconfig.json` from the root of this directory.
-    loadConfig(__dirname)
+              // Load the current `tsconfig.json` from the root of this directory.
+              loadConfig(__dirname)
 
-    // Get the paths from the action's `tsconfig.json`, if any.
-    const paths = actionTsConfig.compilerOptions?.paths ?? {}
+              // Get the paths from the action's `tsconfig.json`, if any.
+              // @ts-expect-error comment-json type mismatch
+              const paths = actionTsConfig?.compilerOptions?.paths ?? {}
 
-    // Add any path mappings from the imported action. Replace the base URL with
-    // the target action path.
-    // @todo Should this take into account the previous `baseUrl` value?
-    register({
-      baseUrl: process.env.TARGET_ACTION_PATH,
-      paths,
-      addMatchAll: true
+              // Add any path mappings from the imported action. Replace the base URL with
+              // the target action path.
+              // @todo Should this take into account the previous `baseUrl` value?
+              register({
+                baseUrl: process.env.TARGET_ACTION_PATH,
+                paths,
+                addMatchAll: true
+              })
+            }
+          }
+        })
+      })
     })
-  }
-}
+  })
+})
